148acb8362719365668e080b276d5e814927a440
Sumit V <sumit.v@logmein.com>
2021-05-31 11:46:09 +0530

    interface and classes

--- public/app.js
+++ public/app.js
@@ -1,41 +1,47 @@
 import { Invoice } from './classes/Invoice.js';
-// Dynamic type
-let something;
-something = 10;
-something = 'some dummy string';
-// Function
-let greet;
-greet = (first, second) => {
-    console.log(first + second);
-};
-const add = (a, b = 10) => a + b;
-const printSomething = () => console.log('Print');
-const greetAgain = (param) => console.log(param);
-// Function signature
-let greetAgainTwice;
-greetAgainTwice = (name, age) => console.log(`${name} - ${age}`);
-let logDetails;
-logDetails = (user) => console.log(`${user.name} - ${user.age}`);
-logDetails({ name: 'testing', age: 20 });
-// DOM
-const invoiceOne = new Invoice('Sam', 'works on the website', 200);
-const invoiceTwo = new Invoice('John', 'works on the website', 280);
-let invoices = [];
-console.log(invoiceOne, invoiceTwo);
+import { Payment } from './classes/Payment.js';
+/** Dynamic type **/
+// let something: any;
+// something = 10;
+// something = 'some dummy string';
+/** Function **/
+// let greet: Function;
+// greet = (first: number, second: number) => {
+//   console.log(first + second);
+// }
+// const add = (a: number, b: number = 10) => a + b;
+// const printSomething = (): void => console.log('Print');
+/**  Type aliases **/
+// type StringOrNumber = string | number;
+// const greetAgain = (param: StringOrNumber) => console.log(param);
+/**  Function signature **/
+// let greetAgainTwice: (first: string, second: number) => void;
+// greetAgainTwice = (name: string, age: number) => console.log(`${name} - ${age}`);
+// type Details = {name: string, age: number};
+// let logDetails: (user: Details) => void;
+// logDetails = (user: Details) => console.log(`${user.name} - ${user.age}`);
+// logDetails({name: 'testing', age: 20})
+/**  DOM  **/
+// const invoiceOne = new Invoice('Sam', 'works on the website', 200);
+// const invoiceTwo = new Invoice('John', 'works on the website', 280);
+// let invoices: Invoice[] = [];
+// console.log(invoiceOne, invoiceTwo)
 const button = document.querySelector('button'); // we are sure that element cannot be undefined ever
 console.log(button.innerHTML);
 const form = document.querySelector('.new-item-form');
 console.log(form);
 const type = document.querySelector('#type');
-const toFrom = document.querySelector('#toFrom');
+const toFrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(amount.valueAsNumber); // because JS by default converts values to string;
+    let doc; // object definitely have implemented format method
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc); // because JS by default converts values to string;
 });
-const testPerson = {
-    name: 'Sam',
-    age: 20
-};
-console.log(testPerson);
--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,53 @@
 import {Invoice} from './classes/Invoice.js';
+import {Payment} from './classes/Payment.js';
+import {IPerson} from './interfaces/IPerson.js'
+import {HasFormatter} from './interfaces/HasFormatter.js';
 
-// Dynamic type
-let something: any;
-something = 10;
-something = 'some dummy string';
+/** Dynamic type **/
 
-
-// Function
-let greet: Function;
-
-greet = (first: number, second: number) => {
-  console.log(first + second);
-}
-
-const add = (a: number, b: number = 10) => a + b;
-
-const printSomething = (): void => console.log('Print');
-
-// Type aliases
-type StringOrNumber = string | number;
-
-const greetAgain = (param: StringOrNumber) => console.log(param);
+// let something: any;
+// something = 10;
+// something = 'some dummy string';
 
 
-// Function signature
-let greetAgainTwice: (first: string, second: number) => void;
-greetAgainTwice = (name: string, age: number) => console.log(`${name} - ${age}`);
 
-type Details = {name: string, age: number};
-let logDetails: (user: Details) => void;
-logDetails = (user: Details) => console.log(`${user.name} - ${user.age}`);
+/** Function **/
 
-logDetails({name: 'testing', age: 20})
+// let greet: Function;
+// greet = (first: number, second: number) => {
+//   console.log(first + second);
+// }
 
-
-// DOM
+// const add = (a: number, b: number = 10) => a + b;
+// const printSomething = (): void => console.log('Print');
 
 
-const invoiceOne = new Invoice('Sam', 'works on the website', 200);
-const invoiceTwo = new Invoice('John', 'works on the website', 280);
-let invoices: Invoice[] = [];
-console.log(invoiceOne, invoiceTwo)
+
+/**  Type aliases **/
+
+// type StringOrNumber = string | number;
+// const greetAgain = (param: StringOrNumber) => console.log(param);
+
+
+
+/**  Function signature **/
+
+// let greetAgainTwice: (first: string, second: number) => void;
+// greetAgainTwice = (name: string, age: number) => console.log(`${name} - ${age}`);
+
+// type Details = {name: string, age: number};
+// let logDetails: (user: Details) => void;
+// logDetails = (user: Details) => console.log(`${user.name} - ${user.age}`);
+// logDetails({name: 'testing', age: 20})
+
+
+/**  DOM  **/
+
+
+// const invoiceOne = new Invoice('Sam', 'works on the website', 200);
+// const invoiceTwo = new Invoice('John', 'works on the website', 280);
+// let invoices: Invoice[] = [];
+// console.log(invoiceOne, invoiceTwo)
 
 const button = document.querySelector('button')!; // we are sure that element cannot be undefined ever
 console.log(button.innerHTML);
@@ -49,24 +56,17 @@ const form = document.querySelector('.new-item-form') as HTMLFontElement;
 console.log(form);
 
 const type = document.querySelector('#type') as HTMLSelectElement;
-const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (event: Event) => {
   event.preventDefault();
-  console.log(amount.valueAsNumber); // because JS by default converts values to string;
+  let doc: HasFormatter; // object definitely have implemented format method
+  if(type.value === 'invoice') {
+    doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+  }
+  console.log(doc); // because JS by default converts values to string;
 })
-
-
-// interfaces
-interface IPerson {
-  name: string;
-  age: number;
-}
-
-const testPerson: IPerson = {
-  name: 'Sam',
-  age: 20
-}
-console.log(testPerson);
